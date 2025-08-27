dba76eed15093867d85652bf932872606a6ca02e
romangiles <romangiles111@gmail.com>
2025-08-27 14:50:27 -0500

    Add files via upload

---
+++ Classwork 1.py
@@ -0,0 +1,12 @@
+student_name = input ("enter the student name: ")
+course_1 = int (input ("Enter the grade for C1") )
+course_2 = int (input ("Enter the grade for C2") )
+course_3 = int (input ("Enter the grade for C3") )
+course_4 = int (input ("Enter the grade for C4") )
+course_5 = int (input ("Enter the grade for C5") )
+
+total=course_1 + course_2 + course_3 + course_4 + course_5
+
+average=(course_1 + course_2 + course_3 + course_4 + course_5)/5
+
+print(average)
