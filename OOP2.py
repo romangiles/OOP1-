c6c5936695157ab5c3795903b0e5c95ba18dcfc4
romangiles <romangiles111@gmail.com>
2025-08-27 14:57:18 -0500

    Add files via upload

---
+++ OOP2.py
@@ -0,0 +1,8 @@
+name= input ("employee name")
+
+wages= int(input("enter the wages"))
+hours= int(input("enter the hours"))
+days= int(input("enter the days"))
+monthy_wages= wages * hours * days
+
+print(monthy_wages)
