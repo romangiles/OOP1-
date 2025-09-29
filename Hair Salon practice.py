7fea3e51ee4a94e4da66cff7e8c82afdfeaab6de
romangiles <romangiles111@gmail.com>
2025-09-29 15:48:35 -0500

    Add files via upload

---
+++ Hair Salon practice.py
@@ -0,0 +1,29 @@
+thisdict = {
+    "Employee": "Anne",
+    "Availability": "a. 9:30, b. 1:15, c. 3:10"
+}
+thisdict1 = {
+    "Employee": "Sarah",
+    "Availability": "d. 8:30, e. 9:55, f. 11:30"
+}
+thisdict2 = {
+    "Employee": "Hannah",
+    "Availability": "g. 1:40, h. 2:30, i. 2:55"
+}
+print(thisdict)
+print(thisdict1)
+print(thisdict2)
+
+a = "9:30"
+b = "1:15"
+c = "3:10"
+d = "8:30"
+e = "9:55"
+f = "11:30"
+g = "1:40"
+h = "2:30"
+i = "2:55"
+
+
+int(input("When is the best time for you?"))
+
