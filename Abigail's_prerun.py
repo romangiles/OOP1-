aff2d353669ef64991a760f252a381321dc6a04a
romangiles <romangiles111@gmail.com>
2025-12-12 13:54:27 -0600

    Create Abigail's_prerun.py

---
+++ Abigail's_prerun.py
@@ -0,0 +1,11 @@
+with open("Hinfo.dat", "wb") as fileA:
+    fileA.close()
+
+with open("Cinfo.dat", "wb") as fileB:
+    fileB.close()
+
+with open("Iinfo.dat", "wb") as fileIH:
+    fileIH.close()
+
+with open("IIinfo.dat", "wb") as fileIC:
+    fileIC.close()
